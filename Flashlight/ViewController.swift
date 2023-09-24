9f19b58ccbcada3055bee550f5beb67de4627cfe
tony-yun <lifeyun24@naver.com>
2023-09-24 22:24:49 +0900

    added icon and icon change when button touched

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    var isOn = false
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let soundURL = Bundle.main.url(forResource: "Sound/switch", withExtension: "wav") {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+                audioPlayer.prepareToPlay()
+            } catch {
+                print("Error initializing AVAudioPlayer: \(error)")
+            }
+        } else {
+            print("Sound file not found.")
+        }
     }
 
-
+    @IBAction func switchTapped(_ sender: Any) {
+        isOn = !isOn
+        audioPlayer.play()
+        
+        switchButton.setImage(isOn ? UIImage(named: "onSwitch") : UIImage(named: "offSwitch") , for: .normal)
+        backgroundImage.image = isOn ? UIImage(named: "onBG") : UIImage(named: "offBG")
+        
+    }
+    
 }
 
