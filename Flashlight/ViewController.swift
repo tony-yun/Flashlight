846ef646369ff704a429ca08d6b3a41946234ffe
tony-yun <lifeyun24@naver.com>
2023-09-25 22:03:05 +0900

    swift flash on, launch screen added

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -31,10 +31,33 @@ class ViewController: UIViewController {
             print("Sound file not found.")
         }
     }
+    
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
 
     @IBAction func switchTapped(_ sender: Any) {
         isOn = !isOn
         audioPlayer.play()
+        toggleTorch(on: isOn)
         
         switchButton.setImage(isOn ? UIImage(named: "onSwitch") : UIImage(named: "offSwitch") , for: .normal)
         backgroundImage.image = isOn ? UIImage(named: "onBG") : UIImage(named: "offBG")
